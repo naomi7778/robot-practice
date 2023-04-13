543cf1ce0423fe8d2678674d3f9d735c45a5752e
naomi7778 <130330267+naomi7778@users.noreply.github.com>
2023-04-13 11:51:34 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,32 @@
-basic.showIcon(IconNames.Skull)
-let a = 0
+input.onButtonPressed(Button.A, function () {
+    music.stopAllSounds()
+    basic.showIcon(IconNames.Skull)
+    a = 0
+    b = 0
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    Tinybit.RGB_Car_Program().clear()
+    Tinybit.RGB_Car_Program().show()
+})
+input.onButtonPressed(Button.B, function () {
+    Tinybit.Music_Car(Tinybit.enMusic.wawawawaa)
+})
 let b = 0
-Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
-Tinybit.RGB_Car_Program().clear()
+let a = 0
+Tinybit.RGB_Car_Program().setBrightness(255)
+Tinybit.RGB_Car_Program().showColor(neopixel.colors(NeoPixelColors.Purple))
 Tinybit.RGB_Car_Program().show()
 basic.forever(function () {
     for (let a = 0; a <= 255; a++) {
         Tinybit.RGB_Car_Big2(0, 0, a)
         Tinybit.RGB_Car_Program().setBrightness(255)
-        Tinybit.RGB_Car_Program().showColor(neopixel.colors(NeoPixelColors.Blue))
+        Tinybit.RGB_Car_Program().showColor(neopixel.colors(NeoPixelColors.Purple))
         Tinybit.RGB_Car_Program().show()
     }
     basic.pause(10)
     for (let b = 0; b <= 255; b++) {
         Tinybit.RGB_Car_Big2(0, 0, 255 - b)
         Tinybit.RGB_Car_Program().setBrightness(255 - b)
-        Tinybit.RGB_Car_Program().showColor(neopixel.colors(NeoPixelColors.Blue))
+        Tinybit.RGB_Car_Program().showColor(neopixel.colors(NeoPixelColors.Indigo))
         Tinybit.RGB_Car_Program().show()
     }
     basic.pause(10)
